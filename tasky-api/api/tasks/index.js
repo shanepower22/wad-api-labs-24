f13bf94dc5c8bc345d0f3406d036abb547e2b47b
Shane <20102592@mail.wit.ie>
2025-01-04 17:55:08 +0000

    Mongo Lab error handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,7 @@
+import asyncHandler from 'express-async-handler';
 import express from 'express';
 import Task from './taskModel';
+
 
 const router = express.Router(); // eslint-disable-line
 
@@ -10,10 +12,10 @@ router.get('/', async (req, res) => {
 });
 
 // create a task
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
+}));
 
 // Update Task
 router.put('/:id', async (req, res) => {
